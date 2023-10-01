5c80518f88035ed473a926315a9f6de81275f6fd
Andrei-progr <96905580+Andrei-progr@users.noreply.github.com>
2023-10-01 19:05:52 +0300

    Add files via upload

---
+++ IncorrectData.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class IncorrectData extends Exception{
+    public IncorrectData() {
+        super();
+    }
+    public IncorrectData(String message) {
+        super(message);
+    }
+
+}
